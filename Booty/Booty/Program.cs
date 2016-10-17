18da84f4c1d3df426f4c981faceb9c554fa14ed6
paulweaver2011 <paulweaver2011@gmail.com>
2016-10-17 11:26:31 -0600

    Poots with Boots!

---
+++ Booty/Booty/Program.cs
@@ -0,0 +1,22 @@
+﻿//I solemnly swear to do no good!
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Booty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString = "Hello, world!";
+            WriteLine(myString);
+
+            ReadKey();
+        }
+    }
+}
